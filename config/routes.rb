5cb4dc82ceb70bcca3ae48962dff1c0ddbfee84a
danny pham <pham.danny.t@gmail.com>
2014-09-14 14:53:51 -0700

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :calendars
+  root to: 'calendars#index'
   post 'twilio/text', to: 'communications#text_message'
   post 'email/calendar_update', to: 'communications#email_calendar_update'
 end
